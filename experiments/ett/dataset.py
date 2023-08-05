89cc70f7ba0a9ac3e0c7557196cbc500abdc4f2b
Teddy Koker <teddy.koker@gmail.com>
2023-08-05 16:35:45 -0400

    ett exps

---
+++ experiments/ett/dataset.py
@@ -0,0 +1,126 @@
+# Modified from https://github.com/moraieu/query-selector
+# Original code from https://github.com/zhouhaoyi/Informer2020/
+import os
+import numpy as np
+import pandas as pd
+
+import torch
+from torch.utils.data import Dataset, DataLoader
+
+from timefeatures import time_features
+
+
+class StandardScaler():
+    def __init__(self):
+        self.mean = 0.
+        self.std = 1.
+    
+    def fit(self, data):
+        self.mean = data.mean(0)
+        self.std = data.std(0)
+
+    def transform(self, data):
+        mean = torch.from_numpy(self.mean).type_as(data).to(data.device) if torch.is_tensor(data) else self.mean
+        std = torch.from_numpy(self.std).type_as(data).to(data.device) if torch.is_tensor(data) else self.std
+        return (data - mean) / std
+
+    def inverse_transform(self, data):
+        mean = torch.from_numpy(self.mean).type_as(data).to(data.device) if torch.is_tensor(data) else self.mean
+        std = torch.from_numpy(self.std).type_as(data).to(data.device) if torch.is_tensor(data) else self.std
+        return (data * std) + mean
+
+class Dataset_ETT_hour(Dataset):
+    def __init__(self, root_path, flag='train', size=None,
+                 features='S', data_path='ETTh1.csv',
+                 target='OT', scale=True, inverse=False, timeenc=0, freq='h'):
+        # size [seq_len, label_len, pred_len]
+        # info
+        if size == None:
+            self.seq_len = 24 * 4 * 4
+            self.label_len = 24 * 4
+            self.pred_len = 24 * 4
+        else:
+            self.seq_len = size[0]
+            self.label_len = size[1]
+            self.pred_len = size[2]
+        # init
+        assert flag in ['train', 'test', 'val']
+        type_map = {'train': 0, 'val': 1, 'test': 2}
+        self.set_type = type_map[flag]
+
+        self.features = features
+        self.target = target
+        self.scale = scale
+        self.inverse = inverse
+        self.timeenc = timeenc
+        self.freq = freq
+
+        self.root_path = root_path
+        self.data_path = data_path
+
+        if not os.path.exists(os.path.join(self.root_path, self.data_path)):
+            # download data
+            import urllib.request 
+            urllib.request.urlretrieve(
+                f"https://github.com/zhouhaoyi/ETDataset/raw/main/ETT-small/{data_path}",
+                os.path.join(self.root_path, self.data_path)
+            )
+            
+        self.__read_data__()
+
+    def __read_data__(self):
+        self.scaler = StandardScaler()
+        df_raw = pd.read_csv(os.path.join(self.root_path,
+                                          self.data_path))
+
+        border1s = [0, 12 * 30 * 24 - self.seq_len, 12 * 30 * 24 + 4 * 30 * 24 - self.seq_len]
+        border2s = [12 * 30 * 24, 12 * 30 * 24 + 4 * 30 * 24, 12 * 30 * 24 + 8 * 30 * 24]
+        border1 = border1s[self.set_type]
+        border2 = border2s[self.set_type]
+
+        if self.features == 'M' or self.features == 'MS':
+            cols_data = df_raw.columns[1:]
+            target_index = list(cols_data).index(self.target)
+            df_data = df_raw[cols_data]
+        elif self.features == 'S':
+            df_data = df_raw[[self.target]]
+
+        if self.scale:
+            train_data = df_data[border1s[0]:border2s[0]]
+            self.scaler.fit(train_data.values)
+            data = self.scaler.transform(df_data.values)
+        else:
+            data = df_data.values
+
+        df_stamp = df_raw[['date']][border1:border2]
+        df_stamp['date'] = pd.to_datetime(df_stamp.date)
+        data_stamp = time_features(df_stamp, timeenc=self.timeenc, freq=self.freq)
+
+        self.data_x = data[border1:border2]
+        if self.inverse:
+            self.data_y = df_data.values[border1:border2]
+        else:
+            self.data_y = data[border1:border2]
+        if self.features == "MS":
+            self.data_y = self.data_y[:, [target_index]]
+
+        self.data_stamp = data_stamp
+
+    def __getitem__(self, index):
+        s_begin = index
+        s_end = s_begin + self.seq_len
+        r_begin = s_end - self.label_len
+        r_end = r_begin + self.label_len + self.pred_len
+
+        seq_x = self.data_x[s_begin:s_end]
+        seq_y = self.data_y[r_begin:r_end]
+        seq_x_mark = self.data_stamp[s_begin:s_end]
+        seq_y_mark = self.data_stamp[r_begin:r_end]
+
+        return seq_x, seq_y, seq_x_mark, seq_y_mark
+
+    def __len__(self):
+        return len(self.data_x) - self.seq_len - self.pred_len + 1
+
+    def inverse_transform(self, data):
+        return self.scaler.inverse_transform(data)
---
+++ experiments/ett/train.py
@@ -0,0 +1,101 @@
+import torch
+from torch.utils.data import DataLoader
+import numpy as np
+
+from model import Transformer
+from dataset import Dataset_ETT_hour
+
+def main():
+
+    torch.manual_seed(0)
+
+    # hyperparams selected from univariate transformer:
+    # https://github.com/moraieu/query-selector/blob/master/settings/tuned/ts_full_u_h1_24.json
+    seq_len = 720
+    pred_len = 24
+    batch_size = 64
+    iterations = 5
+
+    model = Transformer(
+        dim_val=32,
+        dim_attn=128,
+        input_size=1,
+        dec_seq_len=48,
+        out_seq_len=pred_len,
+        output_len=1,
+        n_decoder_layers=3,
+        n_heads=4,
+        n_encoder_layers=3,
+        enc_attn_type="full",
+        dec_attn_type="full",
+        dropout=0.1,
+    )
+
+    train_ds = Dataset_ETT_hour(root_path="./data", data_path="ETTh1.csv", flag="train", size=[seq_len, 0, pred_len], features="S")
+    test_ds = Dataset_ETT_hour(root_path="./data", data_path="ETTh1.csv", flag="test", size=[seq_len, 0, pred_len], features="S")
+
+    train_dl = DataLoader(train_ds, batch_size=batch_size, shuffle=True, drop_last=True, num_workers=4)
+    test_dl = DataLoader(test_ds, batch_size=1, num_workers=4)
+
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+    model.to(device)
+
+    optim = torch.optim.Adam(model.parameters(), lr=0.00005, weight_decay=1e-2)
+
+
+    # train
+    print("training")
+    for iteration in range(iterations):
+        losses = []
+        for (batch_x, batch_y, _batch_x_mark, _batch_y_mark) in train_dl:
+            batch = batch_x.float().to(device)
+            target = batch_y.float().to(device)
+
+            # NOTE shapes:
+            # batch (input) = [batch size, input sequence len = 720, features=1]
+            # target        = [batch size, target sequence len = 24, features=1]
+            # embedding (if get_embedding=True in model()) = [batch_size, input sequence len = 720, dim = 32]
+            result = model(batch, get_embedding=False)
+
+            loss = torch.nn.functional.mse_loss(result.squeeze(2), target.squeeze(2))
+            losses.append(loss.item())
+
+            optim.zero_grad()
+            loss.backward()
+            optim.step()
+
+        print(f"iteration: {iteration}, loss: {torch.tensor(losses).mean():.3f}")
+        
+
+    # test
+    print("testing")
+    preds, trues = [], []
+    with torch.no_grad():
+        for (batch_x, batch_y, batch_x_mark, batch_y_mark) in test_dl:
+            batch = batch_x.float().to(device)
+            target = batch_y.float().to(device)
+            result = model(batch)
+            preds.append(result.cpu().numpy())
+            trues.append(target.cpu().numpy())
+
+    preds = np.array(preds)
+    trues = np.array(trues)
+    print('test shape:', preds.shape, trues.shape)
+    preds = preds.reshape(-1, preds.shape[-2], preds.shape[-1])
+    trues = trues.reshape(-1, trues.shape[-2], trues.shape[-1])
+
+    mae = np.mean(np.abs(preds - trues))
+    mse = np.mean((preds - trues) ** 2)
+    print(f"MSE: {mse:.3f}, MAE: {mae:.3f}")
+
+    
+
+
+        
+
+
+
+
+if __name__ == "__main__":
+    main()
